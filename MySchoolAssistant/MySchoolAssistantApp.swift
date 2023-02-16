ae76d41a1ef02e37376e1ec45701b8dde9df574e
miralouisepaquin <90649621+miralouisepaquin@users.noreply.github.com>
2023-02-16 09:04:05 -0500

    ajout des vues et manager

---
+++ MySchoolAssistant/MySchoolAssistantApp.swift
@@ -0,0 +1,18 @@
+//
+//  MySchoolAssistantApp.swift
+//  MySchoolAssistant
+//
+//  Created by EdwImac03 on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+
+struct MySchoolAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MessagesView()
+        }
+    }
+}
